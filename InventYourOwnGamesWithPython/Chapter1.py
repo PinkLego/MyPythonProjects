b322a31a661f7b61879bb2a0f9fa048bf0b58aa4
Bamidele Baruwa <bamidelebaruwa@outlook.com>
2020-02-01 13:33:11 +0000

    Thanks dad

--- InventYourOwnGamesWithPython/Chapter1.py
+++ InventYourOwnGamesWithPython/Chapter1.py
@@ -1,3 +1,12 @@
 # Sample exercises for chapter1
+# Math expressions
+hardest = (1 + 1) + 3 + (2 * 3) * 2
+print(hardest)
 
-# I don't understand daddy!
+player_name = input("What is your name?")
+
+player_age = input("How old are you?")
+
+
+print(" You entered " + player_name + " and you are " + player_age + " years old ")Bamn
+Thanks dad for helping me!
