7b64f3d641414387dc3e083db06436184939178b
Bamidele Baruwa <bamidelebaruwa@outlook.com>
2020-02-09 16:51:47 +0000

    My programming for today!

--- InventYourOwnGamesWithPython/chapter 1/Expressions.py
+++ InventYourOwnGamesWithPython/chapter 1/Expressions.py
@@ -4,5 +4,5 @@
 
  10-5+6
 
- 2  +          2
+2  +          2
  #something is wrong but I don't know what! :,(
---
+++ InventYourOwnGamesWithPython/chapter 1/Variable.py
@@ -0,0 +1,20 @@
+spam = 15
+spam + 5
+
+spam = 3
+spam + 5
+
+spam = 15
+spam = spam + 5
+spam
+
+spam = 15
+spam = spam + 5
+spam = spam + 5
+spam = spam + 5
+
+bacon = 10
+runny eggs 15
+baked beans = 30
+toast = 3
+spam = bacon + runny eggs + baked beans + toast
