be1a4f35093d1030afc52a9a7a37731ac610bc5c
Bamidele Baruwa <bamidelebaruwa@outlook.com>
2020-09-06 14:30:55 +0100

    -_-

--- InventYourOwnGamesWithPython/All projects/Space arena.py
+++ InventYourOwnGamesWithPython/All projects/Space arena.py
@@ -10,10 +10,18 @@
 import turtle
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
+SCREEN_HEIGHT = 600
 
 wn = turtle.Screen()
 wn.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
-wn.title("Space Arena! by EDM slushii touché")
+wn.title("Space Arena! by EDM boba touché")
 wn.bgcolor("black")
 
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+
+wn.mainloop()
